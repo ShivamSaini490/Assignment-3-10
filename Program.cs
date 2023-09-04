493295b2d4a072c121f98345b22d8cd018b8e475
shivam <shivam@DESKTOP-BT1O840>
2023-09-04 11:14:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+using AssemblyA;
+
+[assembly: InternalsVisibleTo("AssemblyB")]
+
+namespace AssemblyA
+{
+    public class MyClass
+    {
+        internal MyClass()
+        {
+            Console.WriteLine("Internal constructor called from Assembly A");
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method called from Assembly A");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        myObject.PublicMethod();
+    }
+}
